3f866389fd0c50105b5e87a9a639251e752a1ee6
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-12-02 01:13:39 +0300

    add GetMapping

--- src/main/java/com/workintech/sprintday1/controller/AnimalController.java
+++ src/main/java/com/workintech/sprintday1/controller/AnimalController.java
@@ -2,9 +2,12 @@ package com.workintech.sprintday1.controller;
 
 import com.workintech.sprintday1.entity.Animal;
 import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,4 +21,15 @@ public class AnimalController {
     public AnimalController(){
         System.out.println("Animal Created: ( first rendered part)");
     }
+
+    @GetMapping("/")  // tüm animals 'ı çağırdığım liste. şimdilik boş array []
+public List<Animal> findAll(){
+        System.out.println("Get All Animals (@getMapping 'e adres verdiğim anda render olur bu method)");
+        return animals.values().stream().toList();
+    }
+    @GetMapping("/{id}")  //  id ile (ilgili id map'te varsa) value 'nun değerini döner.
+    public Animal find(@PathVariable int id){
+        return animals.get(id);
+    }
+
 }
